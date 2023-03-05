5e6ce77e963a403e4025056bfbc38ed02da7618c
Youngsu Na <dudtnsk54@naver.com>
2023-03-05 17:21:07 +0900

    ㅡmodel making

--- webproject/models.py
+++ webproject/models.py
@@ -3,6 +3,18 @@ from django.db import models
 # Create your models here.
 
 
-class HelloWorld(models.Model):
+class User(models.Model):
     # 공백의 필요성 여부를 null = false를 통해서 알려준다
-    text= models.CharField(max_length=255, null=False)
+    user_email = models.CharField(max_length=30)
+    user_pw = models.CharField(max_length=20)
+
+class Note(models.Model):
+
+    user_name = models.CharField(max_length=45)
+    description = models.CharField(max_length=200)
+
+class PostIt(models.Model):
+
+    anonymous = models.BooleanField
+    writer = models.CharField(max_length=20)
+    content_text = models.TextField
--- webproject/urls.py
+++ webproject/urls.py
@@ -5,5 +5,5 @@ from webproject.views import hello_world
 app_name = "accountapp"
 
 urlpatterns= [
-    path('hello_world/', hello_world, name='hello_world')
+    path('/login', hello_world, name='hello_world')
 ]
